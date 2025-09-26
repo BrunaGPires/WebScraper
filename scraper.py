22fb65992ea685070e4a6cb92878f1d8fa5d9a22
Bruna Pires <bruna.gpires@hotmail.com>
2025-09-26 15:04:08 -0300

    refactor: improve code structure

--- scraper.py
+++ scraper.py
@@ -1,13 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 import json
 
-# proxy
-
-
-key_words = {
-    "london", "europe", "EUIC", "uk", "united kingdom", "england", "lisbon", "natural history museum", "excel center"
-}
+key_words = [
+    "London", "Europe", "EUIC", "UK", "United Kingdom", "England", "Lisbon", "Natural History Museum", "Excel Center", "Excel Centre"
+]
 
 def scraper_serebii():
     serebii_url = "https://serebii.net/"
@@ -18,6 +16,8 @@ def scraper_serebii():
         "accept-language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
         "accept-encoding": "gzip, deflate, br, zstd",
     }
+    
+    all_news = []
 
     try:
         print("Fazendo requisição...")
@@ -29,41 +29,47 @@ def scraper_serebii():
 
         soup = BeautifulSoup(response.text, "html.parser")
 
-        # procura todos elementos com classe 'post'
+        # Procura todos elementos com classe 'post'
         post_elements = soup.find_all(lambda tag: tag.has_attr('class') and 'post' in tag['class'])
+
+        # Inicializa variveis para o json
+        title = ""
+        full_link = ""
+        post_paragraph = []
 
         for i, post in enumerate(post_elements):
             print(f"\n{'='*60}")
             print(f"POST ELEMENT {i+1} - Classes: {post.get('class', [])}")
             print(f"{'='*60}")
 
-            # pega o h2 e p que estão em post
+            # Pega o h2 e p que estão em post
             h2s = post.find_all('h2', recursive=True)
             ps = post.find_all('p', recursive=True)
             for h2 in h2s:
-                print(f"h2: {h2.get_text(strip=True)}")
+                title_text = h2.get_text(strip=True)
+                print(title_text)
+                title = title_text
+                # Pegar o link do post
+                a = h2.find("a")
+                if a and a.has_attr("href"):
+                    link = a["href"]
+                    full_link = f"https://www.serebii.net{link}"
+                    print(full_link)
             for p in ps:
-                print(f"p: {p.get_text(strip=True)}")
+                p_text = p.get_text(strip=True)
+                print(p_text)
+                post_paragraph.append(p_text)
 
-            # entra na div subcat de post 
-            subcats = post.find_all(lambda tag: tag.has_attr('class') and 'subcat' in tag['class'])
-            for j, subcat in enumerate(subcats):
-                print(f"  {'-'*40}")
-                print(f"  SUBCAT {j+1} inside POST {i+1} - Classes: {subcat.get('class', [])}")
+            news_data = {
+                "id" : i + 1,
+                "title" : title,
+                "post_paragraph" : post_paragraph,
+                "link" : full_link
+            }
 
-                # pega o h3 e p que estão em subcat
-                h3s = subcat.find_all('h3', recursive=True)
-                subcat_ps = subcat.find_all('p', recursive=True)
-                for h3 in h3s:
-                    print(f"  h3: {h3.get_text(strip=True)}")
-                for p in subcat_ps:
-                    print(f"  p: {p.get_text(strip=True)}")
-                print(f"  {'-'*40}")
+            all_news.append(news_data)
 
-                
-            print(f"{'='*60}\n")
-
-        return response.text
+        return all_news
     
     except Exception as e:
         print(f"Erro: {e}")
